56a1cb3956348ffd7515c1762e9201f6715bfd8b
charles3399 <bernaldezsay@gmail.com>
2022-02-19 18:52:04 +0800

    added search city functionality

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,20 @@
   <div class="m-4">
     <Title />
     <div class="p-4 mt-1 text-center">
-      <input type="text" placeholder="Search a city" class="p-4 rounded-full w-3/6 shadow-xl">
+      <form @submit.prevent="getWeatherData">
+        <input v-model="cityName" type="text" placeholder="Search a city" class="p-4 rounded-full w-3/6 shadow-xl">
+      </form>
     </div>
     <div v-if="!loading">
-      <WeatherCard :weather="weatherData" />
+      <div class="text-center p-2" v-if="cityFound == false">
+        <h3 class="text-white font-bold text-4xl">There's no such city</h3>
+      </div>
+      <div v-else>
+        <WeatherCard :weather="weatherData" />
+      </div>
     </div>
     <div class="text-center p-2" v-else>
-      <h3 class="text-white font-bold text-4xl">{{ loadingMessage + '...' }}</h3>
+      <h3 class="text-white font-bold text-4xl">{{loadingMessage}} <i :class="icon"></i></h3>
     </div>
   </div>
 </template>
@@ -25,29 +32,37 @@ export default {
     WeatherCard
   },
   setup() {
+    const icon = ref('fa-solid fa-cloud-sun-rain')
     const weatherData = ref([])
-    const cityName = ref('guindulman')
+    const cityFound = ref(false)
+    const cityName = ref(null)
     const apiKey = ref('fdd7b594a9c25f2b77bf2df3cb47f644')
-    const baseURL = ref(`https://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&appid=${apiKey.value}&units=metric`)
     const loading = ref(true)
-    const loadingMessage = ref('Updating')
+    const loadingMessage = ref('Ang buhay ay weather weather lang')
 
     const getWeatherData = async () => {
-      loading.value = true
-      const response = await fetch(baseURL.value)
-      const data = await response.json()
-      cityName.value = data
-      weatherData.value = data
-      loading.value = false
+      try {
+        loading.value = true
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&appid=${apiKey.value}&units=metric`)
+        const data = await response.json()
+        weatherData.value =  data
+        loading.value = false
+        cityFound.value = true
+        loadingMessage.value = 'Searching city'
+        icon.value = 'fa-solid fa-magnifying-glass-location'
+      }
+      catch(error) {
+        alert(error.message)
+        cityFound.value = false
+      }
     }
 
-    getWeatherData()
-
     return {
+      icon,
       weatherData,
+      cityFound,
       cityName,
       apiKey,
-      baseURL,
       loading,
       loadingMessage,
       getWeatherData
