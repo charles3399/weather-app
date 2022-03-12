84781f68d6ae130a8b4e2b1cf83c6c02605eb19e
charles3399 <bernaldezsay@gmail.com>
2022-03-12 20:19:33 +0800

    fixed time for sunset and sunrise

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -46,8 +46,8 @@ export default {
         weather_description: this.weather.weather[0].description,
         max_temperature: Math.round(this.weather.main.temp_max * 10) / 10,
         min_temperature: Math.round(this.weather.main.temp_min * 10) / 10,
-        sunrise_time: moment.unix(this.weather.sys.sunrise).utc().format('LT'),
-        sunset_time: moment.unix(this.weather.sys.sunset).utc().format('LT')
+        sunrise_time: moment.unix(this.weather.sys.sunrise).format('LT'),
+        sunset_time: moment.unix(this.weather.sys.sunset).format('LT')
       }
     }
   },
